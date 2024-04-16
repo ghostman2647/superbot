f8b350787d448307290c7fcad8279c0df874e362
ghostman2647 <166911473+ghostman2647@users.noreply.github.com>
2024-04-16 19:31:15 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const ZONE = "BomberGameZone";
 export const SALT_KEY = "f17e4e44f7bbc229fdc2d2f55728abba";
 export const DATE_OFFSET = 62135596800000;
 export const VERSION_CODE = 24041520;
-export const SERVERS = ["na", "sea", "sa"];
+// export const SERVERS = ["na", "sea", "sa"];
 export const LC = "";
 
 export const WEB3_RPC = "https://polygon-rpc.com";
