b7e859c71db3fbacf9a6ffd76cd1b7bf77606f35
Diogenesco <82326384+Diogenesco@users.noreply.github.com>
2024-04-16 14:00:06 -0300

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 cd ~
-sudo apt update
-sudo apt install git nano htop -y
-
+apt update
+apt install git nano htop -y
 curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get install gcc g++ make -y
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update -y && sudo apt-get install yarn -y
-sudo apt-get install -y nodejs
-sudo apt install build-essential -y
+bash nodesource_setup.sh
+apt-get install gcc g++ make -y
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update -y && apt-get install yarn -y
+apt-get install -y nodejs
+apt install build-essential -y
 
 node -v
 npm -v
 echo "CORONELMAKER: instalando yarn e pm2"
-sudo npm --force install -g yarn pm2
+npm --force install -g yarn pm2
