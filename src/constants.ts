406db7462bf0bc4c02bb49dfdd60f9c78ba2fee7
ghostman2647 <166911473+ghostman2647@users.noreply.github.com>
2024-04-17 11:57:37 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const HOST = "server-sa.bombcrypto.io";
+export const HOST = "sv-game-0.bombcrypto.io";
 export const PORT = 443;
 export const ZONE = "BomberGameZone";
 export const SALT_KEY = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9";
